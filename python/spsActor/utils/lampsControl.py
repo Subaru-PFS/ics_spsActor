4df106b88f1315347fcedb51c23823b3191b9817
Arnaud Le Fur <afur@princeton.astro.edu>
2021-12-28 17:31:30 -0500

    add lampsControl.IISControl

--- python/spsActor/utils/lampsControl.py
+++ python/spsActor/utils/lampsControl.py
@@ -91,6 +91,31 @@ class LampsControl(QThread):
         pass
 
 
+class IISControl(LampsControl):
+    """ Placeholder to handle IIS cmd threading. """
+    # IIS command syntax is a bit different.
+    goCmd = 'iis go'
+    abortCmd = 'iis abort'
+
+    def __init__(self, exp, enuActor):
+        LampsControl.__init__(self, exp, enuActor, threadName=f'iisControl_{enuActor}')
+
+    @threaded
+    def start(self, cmd):
+        """ Full lamp control routine.  """
+        try:
+            # dont wait for ready signal, at least for now.
+            # self.cmdVar = self._waitForReadySignal(cmd)
+            # Wait for the go signal, namely when all shutters are opened.
+            self.waitForGoSignal()
+            # Ask lamp controller to pulse lamps with the configured timing.
+            self._go(cmd)
+
+        except Exception as e:
+            self.abort(cmd)
+            self.exp.abort(cmd, reason=str(e))
+
+
 class ShutterControlled(LampsControl):
     """ Placeholder to handle lamp cmd threading, in that class exposure time is controlled by shutters. """
     waitBeforeOpening = 2
